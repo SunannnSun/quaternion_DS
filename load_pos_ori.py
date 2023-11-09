preliminarily finish the testing and fixed a few bugs; still working on double cover of quaternions

load_pos_ori.py
@@ -1,11 +1,14 @@
 import os
+from tkinter import W
 import numpy as np
 from util import plot_tools
 from scipy.spatial.transform import Rotation as R
-from util import plot_tools, optimize_tools
+from util import plot_tools, optimize_tools, quat_tools
 import matplotlib.pyplot as plt
 from util.gmm import gmm as gmm_class
 from util.quat_tools import *
+from scipy.signal import savgol_filter
+
 
 
 def _get_sequence(seq_file):
@@ -25,6 +28,8 @@ def _get_sequence(seq_file):
         seq = [line.strip() for line in x]
     return seq
 
+
+
 def _angular_velocities(q1, q2, dt):
     """
     https://mariogc.com/post/angular-velocity-quaternions/
@@ -40,6 +45,12 @@ def load_clfd_dataset(task_id=1, num_traj=1, sub_sample=3):
     """
     [num_demos=9, trajectory_length=1000, data_dimension=7] 
     A data point consists of 7 elements: px,py,pz,qw,qx,qy,qz (3D position followed by quaternions in the scalar first format).
+
+    Remark: dataset does not provide time stamp. Hence, in order to compute velocity, we woudl like to determine an appropriate dt.
+    Given each task contains 1000 observations, we can safely assume the time it takes to complete one trajectory is 10 seconds.
+
+    Manually dealing with multiple trajectories by assigning correct angular velocity near start and end of each trajecotry; i.e. apply 
+    Savgol separately for each individual trajectory
     """
 
 
@@ -48,39 +59,47 @@ def load_clfd_dataset(task_id=1, num_traj=1, sub_sample=3):
     filenames   = _get_sequence(seq_file)
     datafile    = os.path.join(dir_path, "dataset", "pos_ori", filenames[task_id])
     
+
     data        = np.load(datafile)[:, ::sub_sample, :]
     L, N, M     = data.shape
+    T = 10
+    dt = T / 1000 * sub_sample
+    N_tot = num_traj * N
 
-    data_flat        = np.zeros((num_traj*N, M))
+
+    q_train = [R.identity()] *  N_tot
+    w_train = [R.identity()] * (N_tot-1)
+
+
     for l in range(num_traj):
-        data_flat[l*N:(l+1)*N, :] = data[l, :, :]
+        data_ori = np.zeros((N, 4))
 
-    data_pos = data_flat[:, :3]
-    data_ori = data_flat[:, 3:]
+        w        = data[l, :, 3 ].copy()
+        xyz      = data[l, :, 4:].copy()
+        data_ori[:, -1]  = w
+        data_ori[:, 0:3] = xyz
+        q_train[l*N: (l+1)*N] = [R.from_quat(q) for q in data_ori.tolist()]
 
-    w     = data_ori[:, 0].copy()
-    xyz   = data_ori[:, 1:].copy()
+        
+        rotvec_l = np.zeros((N-1, 3))
+        for i in range(rotvec_l.shape[0]):
+            q_k   = data[l, i,   3:]
+            q_kp1 = data[l, i+1, 3:]
+            rotvec_l[i, :] = _angular_velocities(q_k, q_kp1, dt)
+        
+        rotvec_l = savgol_filter(rotvec_l, window_length=20, polyorder=2, axis=0, mode="nearest")
 
-    data_ori[:, -1]  = w
-    data_ori[:, 0:3] = xyz
+        if l != num_traj-1:
+            rotvec_l = np.vstack((rotvec_l, rotvec_l[-1, :][np.newaxis, :]))
+            w_train[l*N: (l+1)*N]   = [R.from_rotvec(rotvec_l[j, :]) for j in range(rotvec_l.shape[0])]
+        else:
+            w_train[l*N: (l+1)*N]   = [R.from_rotvec(rotvec_l[j, :]) for j in range(rotvec_l.shape[0])]
 
 
-    q_train = [R.from_quat(q) for q in data_ori.tolist()]
+
     q_init = q_train[0]
     q_att  = q_train[-1]
 
-
-
-    w_train = [R.identity()] * (len(q_train) -1)
-    for i in range(len(w_train)):
-        q_k   = data_flat[i  , 3:]
-        q_kp1 = data_flat[i+1, 3:]
-        w_train[i] = _angular_velocities(q_k, q_kp1, dt=0.1)
-    # w_train.append(dq_dt*0)
-
-
-    w_train = [R.from_euler('xyz', w_k) for w_k in w_train]
     
 
-
-    return q_init, q_att, q_train, w_train
+    return q_init, q_att, q_train, w_train, dt

main.py
@@ -13,11 +13,11 @@ from scipy.spatial.transform import Rotation as R
 ####### GENERATE RANDOM TRAJECTORY ########
 """
 from load_pos_ori import load_clfd_dataset, _angular_velocities
-# q_init, q_att, q_train, w_train = load_clfd_dataset(task_id=2, num_traj=1, sub_sample=3)
+q_init, q_att, q_train, w_train = load_clfd_dataset(task_id=2, num_traj=1, sub_sample=10)
 
 
 
-q_init, q_att, q_train, w_train = traj_generator.generate_traj(K=1, N=80)
+# q_init, q_att, q_train, w_train = traj_generator.generate_traj(K=1, N=80)
 
 # q_arr = quat_tools.list_to_arr(q_train)
 # q_flipped = np.zeros((q_arr.shape))
@@ -45,9 +45,9 @@ q_init, q_att, q_train, w_train = traj_generator.generate_traj(K=1, N=80)
 """
 ############ PERFORM QUAT-DS ############
 """
-quat_ds = quat_ds_class(q_train, w_train, q_att)
-quat_ds.begin()
-q_test = quat_ds.sim(q_init)
+# quat_ds = quat_ds_class(q_train, w_train, q_att)
+# quat_ds.begin()
+# q_test = quat_ds.sim(q_init)
 
 
 """

util/plot_tools.py
@@ -107,7 +107,7 @@ def animate_rotated_axes(R_list, scale=1):
 
 
 
-def plot_quat(q_list):
+def plot_quat(q_list, **argv):
 
     q_list_q = list_to_arr(q_list)
 
@@ -124,6 +124,8 @@ def plot_quat(q_list):
         ax.plot(np.arange(N), q_list_q[:, k], color=colors[k], label = label_list[k])
 
     ax.legend()
+    if "title" in argv:
+        ax.set_title(argv["title"])
 
     fig, axs = plt.subplots(4, 1, figsize=(12, 8))
 
@@ -132,7 +134,9 @@ def plot_quat(q_list):
     for k in range(4):
         axs[k].plot(np.arange(N), q_list_q[:, k], color=colors[k], label = label_list[k])
         axs[k].legend(loc="upper left")
-
+   
+    if "title" in argv:
+            axs[0].set_title(argv["title"])
     # plt.show()
 
 