GMM fixed using the handwritten averaging algorithm; now generating proper posterior probability

quat_ds.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-from util import optimize_tools
+from util import optimize_tools, plot_tools
 from util.quat_tools import *
 from util.gmm import gmm as gmm_class   
 
@@ -26,6 +26,7 @@ class quat_ds:
         self.postProb = gmm.postLogProb(self.q_in)
         self.gmm      = gmm
         self.K        = gmm.K
+        
 
 
     def _optimize(self):
@@ -52,7 +53,7 @@ class quat_ds:
             Control the scale of displacement by setting a celling and floor 
         """    
         
-        N = self.N + 100
+        N = len(self.index_list) + 500
         K = self.K
         A = self.A
         q_att = self.q_att

util/gmm.py
@@ -9,7 +9,6 @@ from .plot_tools import *
 
 class gmm:
     def __init__(self, q_in, q_att, index_list, K_init):
-        
         self.q_in     = q_in
         self.q_att    = q_att
         self.index_list = index_list
@@ -17,12 +16,13 @@ class gmm:
 
         self.N = len(q_in)
         self.M = 4
-        self.q_in_att = riem_log(q_att, q_in)
-
+        
 
 
     def begin(self):
-        assignment_arr = BayesianGaussianMixture(n_components=self.K_init, random_state=2).fit_predict(self.q_in_att)
+        q_in_att = riem_log(self.q_att, self.q_in)
+
+        assignment_arr = BayesianGaussianMixture(n_components=self.K_init, n_init=10, random_state=2).fit_predict(q_in_att)
 
         rearrange_list = []
         for idx, entry in enumerate(assignment_arr):
@@ -38,7 +38,7 @@ class gmm:
         self.K = int(assignment_arr.max()+1)
         self._return_norma_class()
 
-        # plot_gmm(self.q_in, self.index_list, assignment_arr)
+        plot_gmm(self.q_in, self.index_list, assignment_arr)
 
         return self.assignment_arr
 
@@ -65,9 +65,16 @@ class gmm:
         for k in range(self.K):
             q_k      = [q for index, q in enumerate(self.q_in) if self.assignment_arr[index]==k] 
             r_k      = R.from_quat([q.as_quat() for q in q_k])
-            q_k_mean = r_k.mean()
-        
+            # q_k_mean = r_k.mean()
 
+
+            q_k_mean = quat_mean(q_k)
+
+            # print(str(k)+" component")
+            # print(q_avg.as_quat())
+            # print(q_k_mean.as_quat())
+    
+    
             Prior[k]  = len(q_k)/self.N
             Mu[k]     = q_k_mean
             Sigma[k]  = riem_cov(q_k_mean, q_k) + 10E-6 * np.eye(4)

util/plot_tools.py
@@ -163,8 +163,6 @@ def animate_rotated_axes(R_list, scale=1, **argv):
 
 def plot_gmm(q_list, index_list, label):
 
-    N = index_list[-1] + 1
-
     colors = ["r", "g", "b", "k", 'c', 'm', 'y', 'crimson', 'lime'] + [
     "#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(200)]
 
@@ -172,13 +170,13 @@ def plot_gmm(q_list, index_list, label):
     ax = fig.add_subplot()
     ax.figure.set_size_inches(12, 6)
 
+    index_list_interp = _interp_index_list(q_list, index_list, interp=True)
+
     color_mapping = np.take(colors, label)
 
     q_list_q = list_to_arr(q_list)
-
-
     for k in range(4):
-        ax.scatter(index_list, q_list_q[:, k], s=2, c=color_mapping)
+        ax.scatter(index_list_interp, q_list_q[:, k], s=1, c=color_mapping)
 
     ax.set_title("GMM results")
     pass
@@ -199,8 +197,7 @@ def plot_demo(q_list, index_list, interp, **argv):
 
     q_list_q = list_to_arr(q_list)
     for k in range(4):
-        ax.scatter(index_list_interp, q_list_q[:, k], s= 1, color=colors[k], label = label_list[k])
-    ax.legend()
+        ax.scatter(index_list_interp, q_list_q[:, k], s= 1, color=colors[k])
 
     if "title" in argv:
         ax.set_title(argv["title"])